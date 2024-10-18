b3de0f2ee7b95a88128f05d569520dbb304961ab
lmiralles0 <lmiralles@localhost.localdomain>
2024-10-17 21:57:25 -0300

    asd

---
+++ SimulacroParcial_2/src/simulacroparcial_2/SimulacroParcial_2.java
@@ -0,0 +1,17 @@
+
+package simulacroparcial_2;
+
+/**
+ *
+ * @author Luciano Miralles
+ */
+public class SimulacroParcial_2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
---
+++ TP_Practica/src/tp_practica/TP_Practica.java
@@ -0,0 +1,17 @@
+
+package tp_practica;
+
+/**
+ *
+ * @author Luciano Miralles
+ */
+public class TP_Practica {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+
+}
