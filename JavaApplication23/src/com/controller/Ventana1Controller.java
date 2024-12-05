55f4987a836e1d40382dadaab1c3e3773ebec04f
MCvitani <MCvitani@P1-PCLM>
2024-12-05 18:06:02 -0300

    qas

--- JavaApplication23/src/com/controller/Ventana1Controller.java
+++ JavaApplication23/src/com/controller/Ventana1Controller.java
@@ -1,9 +1,18 @@
 
 package com.controller;
 
+import com.datos.Persona;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.control.Alert;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
 
 /**
  * FXML Controller class
@@ -11,9 +20,46 @@ import javafx.fxml.Initializable;
  * @author Luciano Miralles
  */
 public class Ventana1Controller implements Initializable {
-
+    
+    @FXML
+    private AnchorPane anchorV1; 
+    
+    public List<Persona> personas = new ArrayList<>();
+    
+    @FXML
+    private TextField nombreV1, apellidoV1, edadV1;
+    
+    @FXML
+    private void enviarDato(MouseEvent e)
+    {
+        Persona persona = new Persona(nombreV1.getText(), apellidoV1.getText(), Integer.parseInt(edadV1.getText()));
+        personas.add(persona);
+        messages();
+        verificarControls();
+    }
+    
+    private void verificarControls() 
+    {
+        for(Node node : anchorV1.getChildren())
+        {
+            if(node instanceof TextField tx)
+            {
+                tx.clear();
+            } 
+        }
+    }
+    
+    private void messages() 
+    {
+        Alert alert = new Alert(Alert.AlertType.INFORMATION);
+        alert.setTitle("Cargado con exito");
+        alert.show();
+        alert.close();
+    }
     /**
-     * Initializes the controller class.
+     * Initializes the controller cl
+     * @param url
+     * @param rb
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
--- JavaApplication23/src/com/model/Init.java
+++ JavaApplication23/src/com/model/Init.java
@@ -5,13 +5,16 @@
 package com.model;
 
 import com.datos.Persona;
+import java.awt.TextField;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 import javafx.application.Application;
+import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 
 /**
@@ -19,6 +22,7 @@ import javafx.stage.Stage;
  * @author Luciano Miralles
  */
 public class Init extends Application {
+    
     
     public List<Persona> personas;
     
@@ -42,14 +46,15 @@ public class Init extends Application {
     public static void main(String[] args) {
         launch(args);
     }
-    Persona p1 = new Persona("Luciano", "Miralles", 30);
-    Persona p2 = new Persona("Horacio", "Miralles", 27);
-    Persona p3 = new Persona("Maximo", "Miralles", 25);
+    //Persona p1 = new Persona("Luciano", "Miralles", 30);
+    //Persona p2 = new Persona("Horacio", "Miralles", 27);
+    //Persona p3 = new Persona("Maximo", "Miralles", 25);
 
     public Init() {
-        personas.add(p1);
-        personas.add(p2);
-        personas.add(p3);
+        //personas.add(p1);
+        //personas.add(p2);
+        //personas.add(p3);
+        
     }
     
 }
